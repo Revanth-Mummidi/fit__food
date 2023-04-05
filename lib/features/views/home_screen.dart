42dccef0be350c90c0cbb4df77e7e9259289103d
Revanth-Mummidi <mummidirevanth@gmail.com>
2023-04-05 15:20:10 +0530

    added nav bar

--- lib/features/views/home_screen.dart
+++ lib/features/views/home_screen.dart
@@ -1,19 +1,61 @@
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   static final route =
       MaterialPageRoute(builder: (context) => const HomePage());
   const HomePage({super.key});
 
   @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int _selectedIndex = 0;
+  static const TextStyle optionStyle =
+      TextStyle(fontSize: 20, fontWeight: FontWeight.bold);
+  static const List<Widget> _widgetOptions = <Widget>[
+    Text(
+      'Index 0: Home',
+      style: optionStyle,
+    ),
+    Text(
+      'Index 1: Business',
+      style: optionStyle,
+    ),
+    Text(
+      'Index 2: School',
+      style: optionStyle,
+    ),
+    Text(
+      'Index 3: Settings',
+      style: optionStyle,
+    ),
+  ];
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+@override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title: const Text("Fit Food"),
+          title: Row(
+            children: [
+              Text(
+                "Hi, ",
+                style: Theme.of(context).textTheme.headlineMedium,
+              ),
+              Text(
+                "Pavan",
+                style: Theme.of(context).textTheme.headlineMedium,
+              )
+            ],
+          ),
           actions: [
             Icon(
-              Icons.search_rounded,
-              size: 25,
+              Icons.notifications_active_rounded,
+              size: 30,
             )
           ],
         ),
@@ -38,7 +80,8 @@ class HomePage extends StatelessWidget {
                 child: ListTile(
                   title: const Text('Search Foods'),
                   onTap: () {},
-                ),),
+                ),
+              ),
               Card(
                 child: ListTile(
                   title: const Text('LogOut'),
@@ -48,15 +91,37 @@ class HomePage extends StatelessWidget {
             ],
           ),
         ),
-        body:  SingleChildScrollView(
-            child: Column(
-          children: [
-            Card(
-              child: ListTile(
-                title: Text("text item"),
-              ),
-            )
+        bottomNavigationBar: BottomNavigationBar(
+          items: const <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home_max_rounded),
+              label: 'Home',
+              backgroundColor: Color.fromARGB(255, 39, 37, 38),
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.favorite_border_rounded),
+              label: 'Like',
+              backgroundColor: Color.fromARGB(255, 39, 37, 38),
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.food_bank_rounded),
+              label: 'Fit',
+              backgroundColor: Color.fromARGB(255, 39, 37, 38),
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.person_2_rounded),
+              label: 'Profile',
+              backgroundColor: Color.fromARGB(255, 39, 37, 38),
+            ),
           ],
-        )));
+          currentIndex: _selectedIndex,
+         
+          onTap: _onItemTapped,
+        ),
+        body: SingleChildScrollView(
+          child: Center(
+            child: _widgetOptions.elementAt(_selectedIndex),
+          ),
+        ));
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -10,13 +10,12 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
         title: 'Flutter Demo',
-        theme: ThemeData(useMaterial3: true,brightness: Brightness.dark),
-        home: const LoginPage());
+        theme: ThemeData(useMaterial3: true,brightness: Brightness.light),
+        home: const HomePage());
   }
 }
